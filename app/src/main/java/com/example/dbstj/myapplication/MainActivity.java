53526421121aa328ec5e7a6632e07bbbb6b9c6f7
YoonSeokHan <31535194+YoonSeokHan@users.noreply.github.com>
2017-11-25 17:48:29 +0900

    왓수과

---
+++ app/src/main/java/com/example/dbstj/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dbstj.myapplication;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * Created by dbstj on 2017-11-25.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+}
